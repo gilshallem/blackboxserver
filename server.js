d34ac5eb75035025aa2c720f624f8cfae0ac9325
unknown <Gil@.(none)>
2014-02-12 17:06:40 +0200

    fix

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require("express");
-var logfmt = require("logfmt");
+//var logfmt = require("logfmt");
 var app = express();
 
-app.use(logfmt.requestLogger());
+//app.use(logfmt.requestLogger());
 
 app.get('/', function(req, res) {
 	res.send('Hello World!');
@@ -10,5 +10,5 @@ app.get('/', function(req, res) {
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
-	console.log("Listening on " + port);
+	//console.log("Listening on " + port);
 });
