0b5533ef43dda26e0b6bbb797b10e0ece6dd0dac
Gil <forexblackbox2@gmail.com>
2014-07-08 10:36:26 +0300

    fix

--- external_apis/blackboxcrm.js
+++ external_apis/blackboxcrm.js
@@ -10,7 +10,7 @@ exports.notify = function (title,description,data,type,notify,callback) {
 		"description":description,
 		"data":data,
 		"type":type,
-		"notify":notify
+		"notify":notify ? "true" : "false"
 	}, function(err, resp, body) {
 		if (err || resp.statusCode!=200) {
 			callback(-1,err,phone);
